e790d5060693109d859b4d43efb6ff31890fcccf
--add <andrewksmoker@gmail.com>
2021-02-23 15:15:40 -0500

    commit tree ac036591f8246a58e40c93f3b2c4faf716225294

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React from 'react' 
+
+class Bomb extends React.Component{
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    timer = () => {
+        if (this.state.secondsLeft !== 0) {
+            return `${this.state.secondsLeft} seconds left before I go boom!`
+        } else {
+            'Boom!'
+        }
+    }
+
+    render() {
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return (
+      <div>{message}</div>
+    )
+    }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,19 @@
 // your ImageSlider code here!
+import React from 'react' 
+
+class ImageSlider extends React.Component{
+    constructor(props) {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+    render() {
+        let CURRENT_SLIDE = this.state.currentSlideIndex
+        return (
+            <div>I am on slide {CURRENT_SLIDE}</div>
+            )
+    }
+}
+
+export default ImageSlider
